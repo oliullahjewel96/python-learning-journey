f949e13400146b86e1d3def5471963b6e7dbb6bb
oliullahjewel96 <oliullahjewel7@gmail.com>
2026-05-12 17:39:33 +0800

    Different data type conversion

---
+++ week01/calculator.py
@@ -0,0 +1,8 @@
+#Data type conversion
+# x = int(input("Enter the value of X "))
+# y = int(input("Enter the value of y "))
+
+x = float(input("Enter the value of X "))
+y = float(input("Enter the value of y "))
+
+print(f"The result is: {x+y}")
--- week01/hello.py
+++ week01/hello.py
@@ -5,9 +5,13 @@ name = input("What is your name?")
 #Remove whitespace from a str and capitalizing
 
 name = name.strip().title()
+
+#Split's name into first and last name
+first, last = name.split(" ")
+
 #print the output
-print("Hello", name)
+# print("Hello", name)
 
 #format string
 
-print(f"Hello, {name}")
+print(f"Hello, {first}")

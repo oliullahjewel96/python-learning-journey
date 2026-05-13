9c5a593be1b7d53d6f9a983def4201cd807e651f
oliullahjewel96 <oliullahjewel7@gmail.com>
2026-05-13 14:54:12 +0800

    conditionals with comparison operators

---
+++ week01/compare.py
@@ -0,0 +1,24 @@
+# x = int(input("Whats X? "))
+# y = int(input("Whats Y? "))
+
+
+# if x > y: 
+#     print(" x is greater than y")
+# elif x < y:
+#     print("x is less than y")
+# else:
+#     print("x is equal to y")
+
+
+score  = int(input('Score: '))
+
+if score>=90:
+    print("Grade A")
+elif score>=80:
+    print("Grade B")
+elif score>=70:
+    print("Grade C")
+elif score>=60:
+    print("Frade D")
+else: 
+    print("Grade F")
---
+++ week01/parity.py
@@ -0,0 +1,16 @@
+def main():
+    x = int(input("What's X? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("odd")
+
+
+def is_even(n):
+    if n%2 == 0:
+        return True
+    else:
+        return False
+
+
+main()

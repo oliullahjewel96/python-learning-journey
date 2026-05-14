e67d0a34b17a3e499db9f7268e0c0c83ef55b0b3
oliullahjewel96 <oliullahjewel7@gmail.com>
2026-05-14 23:09:05 +0800

    dictionaries inside a list

--- week01/hogwarts.py
+++ week01/hogwarts.py
@@ -8,12 +8,24 @@
 
 
 
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherine"
-}
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor",
+#     "Draco": "Slytherine"
+# }
 
-for student in students:
-    print(student, students[student], sep=",")
+# for student in students:
+#     print(student, students[student], sep=",")
+
+students = [
+     {"name":"Hermione", "house": "Gryffindor", "patronous": "otter"},
+     {"name":"Harry", "house": "Gryffindor", "patronous": "stag"},
+     {"name":"Ron", "house": "Gryffindor", "patronous": "jack russel terrier"},
+     {"name":"Draco", "house": "Slytherine", "patronous": None},
+     
+]
+
+
+for student in range(len(students)):
+    print(student,*students[student].values())
